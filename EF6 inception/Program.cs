aa1fdd998080bf539b5e2a217d35d8b7b97fba01
DimaStanishevskiy <stanishevskiy.dmitro@gmail.com>
2017-02-11 23:32:17 +0200

    task 1

--- EF6 inception/Program.cs
+++ EF6 inception/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace EF6_inception
 {
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+
+    class UserContext : DbContext
+    {
+        public UserContext() : base("DbConnection")
+        {}
+        public DbSet<User> Users { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            using (UserContext db = new UserContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 }; 
+                User user2 = new User { Name = "Sam", Age = 26 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объкты успешно сохранены");
+
+                var users = db.Users;
+                Console.WriteLine("Список объектов: ");
+                foreach(User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+            Console.Read();
         }
     }
 }
